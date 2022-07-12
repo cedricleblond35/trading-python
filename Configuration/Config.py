a84a2207fde251e0c9f7b92f17753bd9f9381cef
Cédric Leblond <cedric.leblond@ovhcloud.com>
2022-07-12 10:05:07 +0200

    modi

--- Configuration/Config.py
+++ Configuration/Config.py
@@ -11,7 +11,7 @@ class Config:
     DEFAULT_XAPI_ADDRESS = 'xapi.xtb.com'
     DEFAULT_XAPI_PORT = 5124
     DEFUALT_XAPI_STREAMING_PORT = 5125
-    USER_ID = "13552266"
+    USER_ID = "13598613"
     PASSWORD = "1976Drick!"
 
     # ------Compte reel------------------------------------------------------
