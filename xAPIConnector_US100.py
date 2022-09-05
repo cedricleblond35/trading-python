33e8a2871407e782dafcee9a9b4eaad9ba14594b
Cédric Leblond <cedric.leblond@ovhcloud.com>
2022-09-05 13:21:12 +0200

    add info tech.

--- xAPIConnector_US100.py
+++ xAPIConnector_US100.py
@@ -547,7 +547,7 @@ async def main():
             spM05_1003 = Supertrend(SYMBOL, "M05", 10, 4)
             superM05_1003T0, superM05_1003T1, superM05_1003T2 = spM05_1003.getST()
 
-            spM05_1003 = Supertrend(SYMBOL, "M01", 10, 3)
+            spM01_1003 = Supertrend(SYMBOL, "M01", 10, 3)
             superM01_1003T0, superM01_1003T1, superM01_1003T2 = spM01_1003.getST()
 
             now = datetime.now()
