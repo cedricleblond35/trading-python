a60195ee7849b8f1068cd5cb9c11785c62c728ce
Cédric Leblond <cedric.leblond@ovhcloud.com>
2022-09-09 15:55:21 +0200

    add info tech.

--- Indicators/Awesome.py
+++ Indicators/Awesome.py
@@ -28,11 +28,11 @@ class Awesome(Price):
     async def __calcul(self):
         print("calcul AW ", self.__timeframe)
 
-        #self._prepareListEMA(0, duration, name)  # toutes les bougies ne possédant pas EMA (HORS LES X PREMIÈRES)
+        self._prepareListEMA(0, self.__MMS2 , "AW")  # toutes les bougies ne possédant pas EMA (HORS LES X PREMIÈRES)
 
 
 
-        print("nombre : ",  len(self._listData)-self.__MMS2)
+        print("nombre : ",  len(self._listData))
         for i in range(self.__MMS2, len(self._listData)):
             list1 = self._listData.copy()[i - self.__MMS2 + 1: i + 1]
             if "AW" not in list(list1)[-1]:
