cdf240c6494ba887809a16fda3ff4ff0f0187efd
Cédric Leblond <cedric.leblond@ovhcloud.com>
2022-09-16 12:56:12 +0200

    Modification info tech.

--- Service/Order.py
+++ Service/Order.py
@@ -343,7 +343,6 @@ def NbrLot(balance, position, stp, vnl):
 
         print("ecart type :", ecartPip)
         nbrelot = round(perteAcceptable / ecartPip / vnl, 2 )
-        print("nbrelot :", nbrelot)
 
         positionMax = round_down(balance*0.80/100/100, 2)
         if nbrelot > positionMax:
