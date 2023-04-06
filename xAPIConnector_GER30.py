d66b15c5ffd0765eedf507c45e26e09a33f65be8
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-04-06 16:07:37 +0200

    smma

--- xAPIConnector_GER30.py
+++ xAPIConnector_GER30.py
@@ -482,7 +482,7 @@ async def main():
                                         tp = round(zoneResistance(tick+15, zone), 1)
                                         price = round(bougie1M01.get("SMMA200"), 1)
                                         comment = "Achat SMMA200"
-                                        o.movebuyLimitWait(trade, sl, tp, price+15, balance, VNL)
+                                        o.movebuyLimitWait(trade, sl, tp, price, balance, VNL)
                                     elif tick > trade["tp"]:
                                         o.delete(trade)
                             elif TransactionSide.SELL_LIMIT == trade['cmd']:
@@ -491,7 +491,7 @@ async def main():
                                         sl = superM01_1003T1
                                         tp = zoneResistanceVente(bougie1M01.get("SMMA200")-15, zone)
                                         price = round(bougie1M01.get("SMMA200"), 1)
-                                        o.moveSellLimitWait(trade, sl, tp,price-15, balance, VNL)
+                                        o.moveSellLimitWait(trade, sl, tp,price, balance, VNL)
                                     elif tick < trade["tp"]:
                                         o.delete(trade)
 
