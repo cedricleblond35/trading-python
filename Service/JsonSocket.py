d7e6759c3e75001fdc40114ab16a9d811658c665
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-04-16 23:02:49 +0200

    smma

--- Service/JsonSocket.py
+++ Service/JsonSocket.py
@@ -13,9 +13,6 @@ class JsonSocket(object):
 
         logger = logging.getLogger()
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-        handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(logging.DEBUG)
         self.log = logger
