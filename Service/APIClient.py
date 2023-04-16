6681ce82f2aff030dc3ffa56c7c0446cba129993
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-04-16 21:49:20 +0200

    smma

--- Service/APIClient.py
+++ Service/APIClient.py
@@ -1,11 +1,13 @@
 from Service.JsonSocket import JsonSocket
 from Configuration.Config import Config
 from Service.Command import Command
+from Configuration.Log import Log
 
 
 class APIClient(JsonSocket):
     def __init__(self, address=Config.DEFAULT_XAPI_ADDRESS, port=Config.DEFAULT_XAPI_PORT, encrypt=True):
         super(APIClient, self).__init__(address, port, encrypt)
+        self.log = Log().logger
         self._command = Command()
         if not self.connect():
             raise Exception(
@@ -25,3 +27,6 @@ class APIClient(JsonSocket):
     def identification(self):
         self._sendObj(self._command.loginCommand())
         return self._readObj()
+
+    def printSocketLog(self):
+        self.log(self.socket)
--- Service/JsonSocket.py
+++ Service/JsonSocket.py
@@ -115,6 +115,7 @@ class JsonSocket(object):
     def is_socket_closed(self) -> bool:
         #https://stackoverflow.com/questions/48024720/python-how-to-check-if-socket-is-still-connected
         print("self.socket:", self.socket)
+        logging.debug(self.socket)
         if not self.socket:
             print("client deconnecté")
             return True
