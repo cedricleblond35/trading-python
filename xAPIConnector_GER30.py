164241c176266785fc8fb98779c0c4e91146fb27
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-04-17 19:08:13 +0200

    smma

--- xAPIConnector_GER30.py
+++ xAPIConnector_GER30.py
@@ -384,7 +384,9 @@ async def main():
                     logger.warning('Login failed. Error code: {0}'.format(loginResponse['errorCode']))
                     return
 
-            if 0 <= j < 5 and 13 < todayPlus2Hours.hour < 15 or 16 < todayPlus2Hours.hour < 18:
+
+            if 0 <= j < 5 and 10 < todayPlus2Hours.min < 14 or 16 < todayPlus2Hours.min < 18:
+            #if 0 <= j < 5 and 13 < todayPlus2Hours.hour < 15 or 16 < todayPlus2Hours.hour < 18:
 
                 ############### calcul des indicateurs ##########################""
                 current_time = today.strftime("%H:%M:%S")
