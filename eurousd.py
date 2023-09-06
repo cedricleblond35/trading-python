54c99985e8b6aa8955ba04eb769628afdede9a52
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-09-06 17:02:08 +0200

    modernisation code eurusd

--- eurousd.py
+++ eurousd.py
@@ -532,11 +532,7 @@ async def main():
         logger.info("mise à jour")
         await majDatAall(logger, email, client, SYMBOL, db)
         logger.info("mise à jour fini")
-        # # # moyen mobile ##################################################################################################
-        moyMobil_05 = MM(SYMBOL, "M05", 0)
-        moyMobil_01 = MM(SYMBOL, "M01", 0)
-        await moyMobil_05.EMA(70, ARRONDI_INDIC)
-        await moyMobil_05.SMMA(200, ARRONDI_INDIC)
+
 
         #
         logger.info("calcul Awesome")
