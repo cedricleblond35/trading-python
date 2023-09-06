c925d4c21a3cb950c0a036a3038be684a7621469
Cédric Leblond <cedric.leblond@ovhcloud.com>
2023-09-06 10:46:01 +0200

    modernisation code eurusd

--- Configuration/Log.py
+++ Configuration/Log.py
@@ -11,7 +11,7 @@ class Log:
     def __formatLogger(self):
         # create a logging format
         handler = logging.FileHandler('mylog.log')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
